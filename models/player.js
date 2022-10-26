93a8a7cebfe6bc03a9597141b1cd8983d0018d7c
sk <jsckhan@outlook.com>
2022-10-26 11:24:19 -0400

    Add model schemas: account, player, team

---
+++ models/player.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const playerSchema = mongoose.Schema({
+	name: String,
+	team: String,
+	summoners: {
+		type: mongoose.SchemaTypes.ObjectId,
+		ref: "gameAccount",
+	},
+});
+
+module.exports = mongoose.model(playerSchema, "player");
