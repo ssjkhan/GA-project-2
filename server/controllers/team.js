9a8246fef752198affd0c8b1c5c2879e24239eb4
sk <jsckhan@outlook.com>
2022-10-28 06:43:52 -0400

    Add new team creation functionality during player sign up

--- server/controllers/team.js
+++ server/controllers/team.js
@@ -1,26 +1,51 @@
-import PlayerModel from "../models/player.js";
+import Player from "../models/player.js";
+import team from "../models/team.js";
+import Team from "../models/team.js";
 
 async function main(req, res, next) {
-	const playersArr = await PlayerModel.find().lean().exec();
+	const playersArr = await Player.find().lean().exec();
 
 	res.render("./team/index", { title: "DraftAtlas", players: playersArr });
 }
 
 async function addPlayer(req, res, next) {
-	console.log(req.body);
+	var teamName = req.body.teamName;
+	var playerName = req.body.playerName;
+	var role = req.body.ROLE;
+
 	try {
-		await PlayerModel.create(req.body);
+		var playerDoc = new Player({
+			playerName: playerName,
+			ROLE: role,
+			team: null,
+		});
+
+		await playerDoc.save();
+		var allPlayers = await Player.find({}).exec();
+		console.log(allPlayers);
+
+		var teamDoc = await Team.find({ teamName: teamName }).exec();
+		if (teamDoc.length === 0) {
+			teamDoc = new Team({ teamName: teamName });
+			teamDoc.save();
+		}
+
+		playerDoc.team = teamDoc._id;
+
+		teamDoc.players.push(playerDoc._id);
 	} catch (error) {
 		console.log(error);
 	}
 	res.redirect("/team");
 }
 
+async function addTeam(req, res, next) {}
+
 async function deletePlayer(req, res, next) {
 	var name = req.query.playerName.slice(1);
 
 	try {
-		await PlayerModel.deleteOne({ playerName: name });
+		await Player.deleteOne({ playerName: name });
 	} catch (error) {
 		console.log(error);
 	}
--- server/models/team.js
+++ server/models/team.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 const teamSchema = mongoose.Schema({
+	teamName: String,
 	players: [
 		{
 			type: mongoose.Schema.Types.ObjectId,
 			ref: "player",
+			default: null,
 		},
 	],
 });
