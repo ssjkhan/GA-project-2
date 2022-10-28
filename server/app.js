6ab56142548f87f31ef37327124802653e425e23
sk <jsckhan@outlook.com>
2022-10-28 00:07:49 -0400

    Staging for index css

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Mount parsing middleware
 app.use(logger("dev"));
