94a46d9017936bb8f80d43e683c00f8a2b3bd99d
sk <jsckhan@outlook.com>
2022-10-28 05:37:55 -0400

    remove export aggregation for test from index controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,6 +2,5 @@ function main(req, res, next) {
 	res.render("./index", { title: "DraftAtlas" });
 }
 
-export { default as test } from "../routes/test.js";
 export { default as team } from "../routes/team.js";
 export { main as default, main };
